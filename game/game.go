2120aef83fa63a32c143f1df87218bef04b8eefd
Dae314 <discord@dae314>
2024-03-27 17:33:09 -1000

    Create game simulation

--- game/game.go
+++ game/game.go
@@ -1,9 +1,174 @@
 package game
 
+import (
+	"fmt"
+	"math/rand"
+)
+
+const deckSize = 1000
+const slotSize = 20
+
+type GameState int
+
+const (
+	DrawState GameState = iota
+	PlaceState
+	WinState
+	LoseState
+)
+
+type ErrGameInvalidState struct {
+	state  GameState
+	action string
+}
+
+func (e *ErrGameInvalidState) Error() string {
+	stateStrings := []string{"DrawState", "PlaceState", "WinState", "LoseState"}
+	return fmt.Sprintf("State %s is not a valid state for %s", stateStrings[e.state], e.action)
+}
+
+type ErrPlaceOutofBounds struct {
+	index int
+}
+
+func (e *ErrPlaceOutofBounds) Error() string {
+	return fmt.Sprintf("Attempted to place at index %d, which is not a valid placement slot.", e.index)
+}
+
 type PlaceItGame struct {
-	slots []int
+	Slots      []int
+	State      GameState
+	CurDraw    int
+	ValidSlots []int
+	Deck       []int
 }
 
 func NewGame() PlaceItGame {
-	return PlaceItGame{slots: make([]int, 20)}
+	deck := make([]int, deckSize)
+	n := 1
+	for i := range deck {
+		deck[i] = n
+		n++
+	}
+	slots := make([]int, slotSize)
+	for i := range slots {
+		slots[i] = -1
+	}
+	return PlaceItGame{
+		Slots:   slots,
+		State:   DrawState,
+		CurDraw: -1,
+		Deck:    deck,
+	}
+}
+
+func (g *PlaceItGame) Draw() error {
+	if g.State != DrawState {
+		return &ErrGameInvalidState{state: g.State, action: "Draw"}
+	}
+	randIdx := rand.Intn(len(g.Deck))
+	g.CurDraw = g.Deck[randIdx]
+	g.Deck = removeFromSlice(g.Deck, randIdx)
+	g.ValidSlots = validSlots(g)
+	g.State = checkGameover(g)
+	return nil
+}
+
+func (g *PlaceItGame) Place(i int) error {
+	if g.State != PlaceState {
+		return &ErrGameInvalidState{state: g.State, action: "Place"}
+	}
+	if !contains(g.ValidSlots, i) {
+		return &ErrPlaceOutofBounds{index: i}
+	}
+	if g.Slots[i] != -1 {
+		return &ErrPlaceOutofBounds{index: i}
+	}
+	g.Slots[i] = g.CurDraw
+	g.CurDraw = -1
+	g.State = DrawState
+	return nil
+}
+
+func validSlots(g *PlaceItGame) []int {
+	type pair struct {
+		index int
+		value int
+	}
+	var validSlots []int
+	var vals []pair
+	empty := true
+	for i, v := range g.Slots {
+		if v > 0 {
+			vals = append(vals, pair{i, v})
+			empty = false
+		}
+	}
+	if empty {
+		return []int{
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+			10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+		}
+	}
+	leftMax := -1
+	leftIndex := -1
+	rightMin := -1
+	rightIndex := -1
+	for _, v := range vals {
+		if v.value < g.CurDraw && (leftMax == -1 || v.value > leftMax) {
+			leftMax = v.value
+			leftIndex = v.index
+		}
+		if v.value > g.CurDraw && (rightMin == -1 || v.value < rightMin) {
+			rightMin = v.value
+			rightIndex = v.index
+		}
+	}
+
+	if leftMax == -1 && rightMin != -1 {
+		for i := 0; i < rightIndex; i++ {
+			if g.Slots[i] == -1 {
+				validSlots = append(validSlots, i)
+			}
+		}
+	} else if leftMax != -1 && rightMin == -1 {
+		for i := leftIndex; i < len(g.Slots); i++ {
+			if g.Slots[i] == -1 {
+				validSlots = append(validSlots, i)
+			}
+		}
+	} else {
+		for i := leftIndex; i < rightIndex; i++ {
+			if g.Slots[i] == -1 {
+				validSlots = append(validSlots, i)
+			}
+		}
+	}
+	return validSlots
+}
+
+func checkGameover(g *PlaceItGame) GameState {
+	if len(g.ValidSlots) != 0 {
+		return PlaceState
+	}
+	for _, v := range g.Slots {
+		if v == -1 {
+			return LoseState
+		}
+	}
+	return WinState
+}
+
+func removeFromSlice(s []int, i int) []int {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func contains[T comparable](s []T, e T) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
--- game/game_test.go
+++ game/game_test.go
@@ -7,9 +7,267 @@ import (
 
 func TestNewGame(t *testing.T) {
 	game := NewGame()
-	gameType := reflect.TypeOf(game).String()
+	checkType(t, game, "game.PlaceItGame")
+}
 
-	if gameType != "game.PlaceItGame" {
-		t.Errorf("Expected game to be type game.PlaceItGame, but it is %q", gameType)
+func TestDraw(t *testing.T) {
+	game := NewGame()
+	t.Run("Draw in DrawState", func(t *testing.T) {
+		err := game.Draw()
+
+		checkEquals(t, err, nil)
+		checkEquals(t, game.State, PlaceState)
+		checkNotNil(t, game.CurDraw)
+	})
+	t.Run("Draw in PlaceState", func(t *testing.T) {
+		err := game.Draw()
+
+		checkType(t, err, "*game.ErrGameInvalidState")
+	})
+}
+
+func TestPlace(t *testing.T) {
+	slots := []int{
+		-1, -1, 120, -1, -1, 200, -1, -1, -1, 450,
+		-1, -1, -1, 750, -1, -1, -1, 960, 980, -1,
+	}
+	t.Run("Place in DrawState", func(t *testing.T) {
+		game := PlaceItGame{
+			Slots: slots,
+			State: DrawState,
+		}
+		err := game.Place(9)
+
+		checkType(t, err, "*game.ErrGameInvalidState")
+	})
+	t.Run("Place in PlaceState", func(t *testing.T) {
+		game := PlaceItGame{
+			Slots:      slots,
+			State:      PlaceState,
+			CurDraw:    145,
+			ValidSlots: []int{3, 4},
+		}
+		err := game.Place(3)
+
+		checkEquals(t, err, nil)
+		checkEquals(t, game.State, DrawState)
+		checkEquals(t, game.CurDraw, -1)
+	})
+	t.Run("Place Out of Bounds", func(t *testing.T) {
+		game := PlaceItGame{
+			Slots:   slots,
+			State:   PlaceState,
+			CurDraw: 145,
+		}
+		err := game.Place(100)
+
+		checkNotNil(t, err)
+		if err != nil {
+			checkType(t, err, "*game.ErrPlaceOutofBounds")
+		}
+	})
+	t.Run("Place in Invalid Slot", func(t *testing.T) {
+		game := PlaceItGame{
+			Slots:   slots,
+			State:   PlaceState,
+			CurDraw: 145,
+		}
+		err := game.Place(1)
+
+		checkNotNil(t, err)
+		if err != nil {
+			checkType(t, err, "*game.ErrPlaceOutofBounds")
+		}
+	})
+}
+
+func TestValidSlots(t *testing.T) {
+	slots := []int{
+		-1, -1, 120, -1, -1, 200, -1, -1, -1, 450,
+		-1, -1, -1, 750, -1, -1, -1, 960, 980, -1,
+	}
+	t.Run("Test Middle Draw", func(t *testing.T) {
+		middleDraw := PlaceItGame{
+			Slots:   slots,
+			State:   PlaceState,
+			CurDraw: 145,
+		}
+		got := validSlots(&middleDraw)
+		want := []int{3, 4}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("Test Leading Edge Draw", func(t *testing.T) {
+		leadEdgeDraw := PlaceItGame{
+			Slots:   slots,
+			State:   PlaceState,
+			CurDraw: 15,
+		}
+		got := validSlots(&leadEdgeDraw)
+		want := []int{0, 1}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("Test Tail Edge Draw", func(t *testing.T) {
+		tailEdgeDraw := PlaceItGame{
+			Slots:   slots,
+			State:   PlaceState,
+			CurDraw: 990,
+		}
+		got := validSlots(&tailEdgeDraw)
+		want := []int{19}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("Test Losing Draw", func(t *testing.T) {
+		losingDraw := PlaceItGame{
+			Slots:   slots,
+			State:   PlaceState,
+			CurDraw: 975,
+		}
+		got := validSlots(&losingDraw)
+		var want []int
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("Test Empty Draw", func(t *testing.T) {
+		emptySlots := []int{
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		}
+		emptyDraw := PlaceItGame{
+			Slots:   emptySlots,
+			State:   PlaceState,
+			CurDraw: 515,
+		}
+		got := validSlots(&emptyDraw)
+		want := []int{
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+			10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("Test single number left", func(t *testing.T) {
+		singleSlots := []int{
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, 500,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		}
+		singleDraw := PlaceItGame{
+			Slots:   singleSlots,
+			State:   PlaceState,
+			CurDraw: 150,
+		}
+		got := validSlots(&singleDraw)
+		want := []int{
+			0, 1, 2, 3, 4, 5, 6, 7, 8,
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("Test single number right", func(t *testing.T) {
+		singleSlots := []int{
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, 500,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		}
+		singleDraw := PlaceItGame{
+			Slots:   singleSlots,
+			State:   PlaceState,
+			CurDraw: 750,
+		}
+		got := validSlots(&singleDraw)
+		want := []int{
+			10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestCheckGameover(t *testing.T) {
+	t.Run("Test Game Win", func(t *testing.T) {
+		winSlots := []int{
+			21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+			31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+		}
+		winningGame := PlaceItGame{
+			Slots:      winSlots,
+			State:      PlaceState,
+			CurDraw:    111,
+			ValidSlots: []int{},
+		}
+		got := checkGameover(&winningGame)
+		want := WinState
+
+		checkEquals(t, got, want)
+	})
+	t.Run("Test Game Lose", func(t *testing.T) {
+		loseSlots := []int{
+			90, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		}
+		losingGame := PlaceItGame{
+			Slots:      loseSlots,
+			State:      PlaceState,
+			CurDraw:    5,
+			ValidSlots: []int{},
+		}
+		got := checkGameover(&losingGame)
+		want := LoseState
+
+		checkEquals(t, got, want)
+	})
+	t.Run("Test Not Gameover", func(t *testing.T) {
+		normalSlots := []int{
+			90, -1, -1, -1, -1, 500, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, 901, -1, -1,
+		}
+		normalGame := PlaceItGame{
+			Slots:      normalSlots,
+			State:      PlaceState,
+			CurDraw:    111,
+			ValidSlots: []int{1, 2, 3, 4},
+		}
+		got := checkGameover(&normalGame)
+		want := PlaceState
+
+		checkEquals(t, got, want)
+	})
+}
+
+func checkType[T any](t testing.TB, got T, want string) {
+	t.Helper()
+	gotType := reflect.TypeOf(got).String()
+	if reflect.TypeOf(got).String() != want {
+		t.Errorf("got variable of type %q want variable of type %q", gotType, want)
+	}
+}
+
+func checkEquals[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func checkNotNil[T comparable](t testing.TB, got T) {
+	t.Helper()
+	var zero T
+	if got == zero {
+		t.Errorf("expected %v to not be nil", got)
 	}
 }
